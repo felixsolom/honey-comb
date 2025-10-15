e8f1453a23fd1d07f8753c094560a02dea46855f
felixsolom <felix.solom@gmail.com>
2025-10-15 18:40:55 +0300

    run python tests

---
+++ functions/run_python.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+
+
+def run_python_file(working_directory: str , file_path: str, args=[]) -> str:
+    full_path = os.path.join(working_directory, file_path)
+    absolute_path = os.path.abspath(full_path)
+
+    if not absolute_path.startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.exists(absolute_path):
+        return f'Error: File "{file_path}" not found.'
+    
+    if not file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    
+    try:
+        cmd = ['uv', 'run', absolute_path] + args 
+        completed_process = subprocess.run(
+            cmd,
+            capture_output=True,
+            text=True,
+            timeout=30,
+            cwd=working_directory
+        )
+
+        output = []
+        if completed_process.stdout:
+            output.append(f'STDOUT:\n {completed_process.stdout}')
+
+        if completed_process.stderr:
+            output.append(f'STDERR:\n {completed_process.stderr}')
+
+        if completed_process.returncode != 0:
+            output.append(f'Process exited with code: {completed_process.returncode}')
+
+        return '/n'.join(output) if output else 'No output produced'
+    except Exception as e:
+        return f'Error: executing Python file: {e}'
+    
+
+    
+
+
+
--- tests.py
+++ tests.py
@@ -1,19 +1,16 @@
 import unittest
-import textwrap
 from functions.get_files_info import get_files_info, get_file_content, write_file
+from functions.run_python import run_python_file
 
 class TestFileInfo(unittest.TestCase):
-
    def test_error2_dir(self):
         result = get_files_info("calculator", "../")
-        print(result)
         expected = 'Error: Cannot list "../" as it is outside the permitted working directory'
         self.assertEqual(result, expected)
 
 class TestFileContent(unittest.TestCase):
    def test_main(self):
       result = get_file_content("calculator", "main.py")
-      print(result)
       with open("calculator/main.py", "r") as f:
          expected = f.read(10000)
         
@@ -21,7 +18,6 @@ class TestFileContent(unittest.TestCase):
 
    def test_calc(self):
       result = get_file_content("calculator", "pkg/calculator.py")
-      print(result)
       with open("calculator/pkg/calculator.py", "r") as f:
          expected = f.read(10000)
         
@@ -29,14 +25,12 @@ class TestFileContent(unittest.TestCase):
        
    def test_error(self):
       result = get_file_content("calculator", "/bin/cat")
-      print(result)
       expected = 'Error: Cannot read "/bin/cat" as it is outside the permitted working directory'
         
       self.assertEqual(result, expected)  
 
    def test_error2(self):
       result = get_file_content("calculator", "pkg/does_not_exist.py")
-      print(result)
       expected = 'Error: File not found or is not a regular file: "pkg/does_not_exist.py"'
         
       self.assertEqual(result, expected)  
@@ -44,24 +38,56 @@ class TestFileContent(unittest.TestCase):
 class TestWriteFile(unittest.TestCase):
    def test_lorem(self):
       result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-      print(result)
       expected = f'Successfully wrote to "lorem.txt" (28 characters written)'
 
       self.assertEqual(result, expected)
 
    def test_more_lorem(self):
       result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-      print(result)
       expected = f'Successfully wrote to "pkg/morelorem.txt" (26 characters written)'
 
       self.assertEqual(result, expected)
 
    def test_error(self):
       result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-      print(result)
       expected = 'Error: Cannot write to "/tmp/temp.txt" as it is outside the permitted working directory'
 
       self.assertEqual(result, expected)
+
+class TestRunPython(unittest.TestCase):
+   def test_run_main(self):
+      result = run_python_file("calculator", "main.py")
+      print(result)
+   
+   def test_args(self):
+      result = run_python_file("calculator", "main.py", ["3 + 5"])
+      print(result)
+
+   def test_tests(self):
+      result = run_python_file("calculator", "tests.py")
+      print(result)
+
+   def test_error(self):
+      result = run_python_file("calculator", "../main.py")
+      print(result)
+      expected = 'Error: Cannot execute "../main.py" as it is outside the permitted working directory'
+
+      self.assertEqual(result, expected)
+
+   def test_error_non_existent_dir(self):
+      result = run_python_file("calculator", "nonexistent.py")
+      print(result)
+      expected = 'Error: File "nonexistent.py" not found.'
+
+      self.assertEqual(result, expected)
+
+   def test_error_not_python_file(self):
+      result = run_python_file("calculator", "lorem.txt")
+      print(result)
+      expected = 'Error: "lorem.txt" is not a Python file.'
+
+      self.assertEqual(result, expected)
+
 
 
 if __name__ == "__main__":
